114e38ce81b6ded6d3ece1b532f506602444a53c
Giovanni Lovato <heruan@aldu.net>
2014-03-20 11:32:17 +0100

    Initial commit

---
+++ src/Logger.php
@@ -0,0 +1,19 @@
+<?php
+namespace Bread\Console;
+
+use Bread\Console\Colors;
+
+class Logger
+{
+    private $colors;
+
+    public function __construct()
+    {
+        $this->colors = new Colors();
+    }
+
+    public function log($string, $fgColor = null, $bgColor = null)
+    {
+        print $this->colors->getColoredString($string, $fgColor, $bgColor);
+    }
+}
